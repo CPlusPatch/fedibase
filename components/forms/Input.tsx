031d1622c634b89aabcc0fc484d7b472ffb1e4e1
Gaspard Wierzbinski <contact@cpluspatch.com>
2023-02-11 15:48:45 -1000

    Add proper login instead of token login

--- components/forms/Input.tsx
+++ components/forms/Input.tsx
@@ -1,16 +1,30 @@
+import { DetailedHTMLProps, InputHTMLAttributes } from "react";
 import { classNames } from "utils/functions";
 
-export function Input({ isLoading, children, name, type = "text",id = undefined, className = "", ...props }) {
+interface InputOptions {
+	isLoading: boolean;
+	children: any;
+	name: string;
+}
+
+export function Input({
+	isLoading,
+	children,
+	name,
+	className = "",
+	...props
+}: InputOptions & DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>) {
 	return (
 		<div>
 			{children}
 			<div className="mt-1">
 				<input
-					id={id}
 					name={name}
 					disabled={isLoading}
-					type={type}
-					className={classNames("block px-3 py-2 w-full placeholder-gray-400 rounded-md border border-gray-300 shadow-sm duration-200 appearance-none outline-none disabled:bg-gray-100 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm", className)}
+					className={classNames(
+						"block px-3 py-2 w-full placeholder-gray-400 rounded-md border border-gray-300 shadow-sm duration-200 appearance-none outline-none disabled:bg-gray-100 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm",
+						className,
+					)}
 					{...props}
 				/>
 			</div>
--- components/login/Login.tsx
+++ components/login/Login.tsx
@@ -6,37 +6,87 @@ import Link from "next/link";
 import Button from "components/buttons/Button";
 import MetaTags from "components/head/MetaTags";
 import { setToLocalStorage } from "utils/functions";
-import { setCookie } from "cookies-next";
+import { getCookie, setCookie } from "cookies-next";
 import { Input, Label } from "components/forms/Input";
-import generator, { detector } from "megalodon";
+import generator, { detector, OAuth } from "megalodon";
 
 export default function Login() {
 	const [isLoading, setIsLoading] = useState<boolean>(false);
+
+	const [mode, setMode] = useState<"login" | "code"> ("login");
+	const [data, setData] = useState<{
+		instanceType: string,
+		instanceUrl: string,
+		clientId: string,
+		clientSecret: string,
+		handle: string,
+	}>();
 
 	const loginForm = async (event: any) => {
 		event.preventDefault();
 		setIsLoading(true);
 
-		setCookie("instanceUrl", event.target["url"].value);
-		setCookie("handle", event.target["handle"].value);
-		setCookie("accessToken", event.target["token"].value);
+		const handle = event.target["handle"].value;
+
+		const instanceUrl = "https://" + handle.split("@")[2];
+
+		setCookie("instanceUrl", instanceUrl);
+		setCookie("handle", handle);
 
 		const instanceType = event.target["type"].value;
 		setCookie("instanceType", instanceType);
 
-		const client = generator(instanceType, event.target["url"].value, event.target["token"].value);
+		const client = generator(instanceType, instanceUrl);
 
-		// Find ID of logged in account :( im sowwy
-		const accountIds = await client.searchAccount(event.target["handle"].value);
+		const appData = await client.registerApp("Fedibase Web", {
+			scopes: [
+				"read", "write", "follow"
+			]
+		});
+
+		const { clientId, clientSecret, url } = appData;
+
+		setData({
+			clientId: clientId,
+			clientSecret: clientSecret,
+			instanceType: instanceType,
+			instanceUrl: instanceUrl,
+			handle: handle
+		});
+
+		window.open(url);
+
+		setMode("code");
+		setIsLoading(false);
+	}
+
+	const loginWithCode = async (event: any) => {
+		event.preventDefault();
+		//setIsLoading(true);
 		
-		accountIds.data.map(account => {
-			// bad hack but IT WORKS!!
-			if (account.acct == event.target["handle"].value.split("@")[1]) {
-				setCookie("accountId", account.id);
+		const code = event.target["code"].value;
 
-				setIsLoading(false);
-				window.location.reload();
-			}
+		const client = generator(data.instanceType as any, data.instanceUrl);
+
+		client.fetchAccessToken(data.clientId, data.clientSecret, code).then(async (tokenData: OAuth.TokenData) => {
+			setCookie("accessToken", tokenData.accessToken);
+
+			// Needed in case instance restricts searching to authenticated users
+			const client = generator(data.instanceType as any, data.instanceUrl, tokenData.accessToken);
+
+			// Find ID of logged in account :( im sowwy
+			const accountIds = await client.searchAccount(data.handle);
+			
+			accountIds.data.map(account => {
+				// bad hack but IT WORKS!!
+				console.log(data.handle);
+				if (account.acct == data.handle.split("@")[1]) {
+					setCookie("accountId", account.id);
+
+					setIsLoading(false);
+					window.location.reload();
+				}
+			});
 		});
 	}
 	
@@ -56,60 +106,70 @@ export default function Login() {
 
 				<div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
 					<div className="px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10 font-inter">
-						<form className="space-y-6" action="#" method="POST" onSubmit={loginForm}>
-							<Input
-								id="url"
-								name="url"
-								type="url"
-								autoComplete="url"
-								required
-								placeholder="https://fedi.cpluspatch.com"
-								isLoading={isLoading}
-								className="block px-3 py-2 w-full placeholder-gray-400 rounded-md border border-gray-300 shadow-sm duration-200 appearance-none disabled:bg-gray-100 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm">
-								<Label>Instance URL</Label>
-							</Input>
+						{mode == "login" ? (
+							<form
+								className="space-y-6"
+								action="#"
+								method="POST"
+								onSubmit={loginForm}>
+								<Input
+									id="handle"
+									name="handle"
+									type="handle"
+									autoComplete="url"
+									required
+									placeholder="@cpluspatch@fedi.cpluspatch.com"
+									isLoading={isLoading}
+									className="block px-3 py-2 w-full placeholder-gray-400 rounded-md border border-gray-300 shadow-sm duration-200 appearance-none disabled:bg-gray-100 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm">
+									<Label>Fedi handle</Label>
+								</Input>
 
-							<Input
-								id="handle"
-								name="handle"
-								type="text"
-								required
-								placeholder="@cpluspatch@fedi.cpluspatch.com"
-								isLoading={isLoading}
-								className="block px-3 py-2 w-full placeholder-gray-400 rounded-md border border-gray-300 shadow-sm duration-200 appearance-none disabled:bg-gray-100 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm">
-								<Label>Fedi handle</Label>
-							</Input>
+								<select id="type" name="type">
+									<option value="pleroma">pleroma</option>
+									<option value="mastodon">mastodon</option>
+									<option value="misskey">misskey</option>
+								</select>
 
-							<Input
-								id="token"
-								name="token"
-								type="password"
-								required
-								isLoading={isLoading}
-								className="block px-3 py-2 w-full placeholder-gray-400 rounded-md border border-gray-300 shadow-sm duration-200 appearance-none disabled:bg-gray-100 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm">
-								<Label>Token</Label>
-							</Input>
+								<div>
+									<Button
+										type="submit"
+										ringColor="orange-500"
+										style="orange"
+										className="w-full"
+										isLoading={isLoading}>
+										Sign in
+									</Button>
+								</div>
+							</form>
+						) : (
+							<form
+								className="space-y-6"
+								action="#"
+								method="POST"
+								onSubmit={loginWithCode}>
+								<Input
+									id="code"
+									name="code"
+									type="password"
+									autoComplete={""}
+									required
+									isLoading={isLoading}
+									className="block px-3 py-2 w-full placeholder-gray-400 rounded-md border border-gray-300 shadow-sm duration-200 appearance-none disabled:bg-gray-100 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm">
+									<Label>Code</Label>
+								</Input>
 
-							<select id="type" name="type">
-								<option value="pleroma">pleroma</option>
-								<option value="mastodon">mastodon</option>
-								<option value="misskey">misskey</option>
-							</select>
-
-							<div>
-								<Button
-									type="submit"
-									ringColor="orange-500"
-									style="orange"
-									className="w-full">
-									{isLoading ? (
-										<Spinner className="w-[1.1rem] h-[1.1rem] text-orange-500 fill-white" />
-									) : (
-										<>Sign in</>
-									)}
-								</Button>
-							</div>
-						</form>
+								<div>
+									<Button
+										type="submit"
+										ringColor="orange-500"
+										style="orange"
+										className="w-full"
+										isLoading={isLoading}>
+										Submit
+									</Button>
+								</div>
+							</form>
+						)}
 					</div>
 				</div>
 			</div>
