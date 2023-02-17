7390a7b34deed1d448d746e8a9a12955fffbcd28
Gaspard Wierzbinski <contact@cpluspatch.com>
2023-02-16 17:34:10 -1000

    fix minor syntax mistake

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,27 +2,27 @@ import MetaTags from "components/head/MetaTags";
 import { HomeFeed } from "components/feed/HomeFeed";
 import Nav from "components/sidebar/Nav";
 import MainLayout from "components/layout/MainLayout";
-import { useContext, useEffect } from "react";
+import { useCallback, useContext, useEffect } from "react";
 import { StateContext } from "components/context/StateContext";
 import { Conversation } from "components/feed/Conversation";
 
 const Home = () => {
 	const [state, setState]: any = useContext(StateContext);
 
-	const handlePopState = e => {
+	const handlePopState = useCallback(e => {
 		e.preventDefault();
 		setState(s => ({
 			...s,
 			params: {}
 		}));
 		history.pushState(null, null, "/");
-	};
+	}, [setState]);
 
 	useEffect(() => {
 		window.addEventListener("popstate", handlePopState)
 
 		return () => window.removeEventListener("popstate", handlePopState);
-	}, [])
+	}, [handlePopState])
 
 	return (
 		<div className="relative bg-gray-50 font-inter">
