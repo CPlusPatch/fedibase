337c691eb621c6c8cba0e6134a233f0afa94fbf1
Gaspard Wierzbinski <contact@cpluspatch.com>
2023-02-17 23:08:05 -1000

    Make some transitions faster

--- components/nav/MobileNavbar.tsx
+++ components/nav/MobileNavbar.tsx
@@ -99,19 +99,19 @@ export default function MobileNavbar() {
 							<div className="flex fixed inset-y-0 right-0 ml-10 max-w-full pointer-events-none">
 								<Transition.Child
 									as={Fragment}
-									enter="transform transition ease-in-out duration-500 sm:duration-700"
+									enter="transform transition ease-in-out duration-300 sm:duration-700"
 									enterFrom="translate-x-full"
 									enterTo="translate-x-0"
-									leave="transform transition ease-in-out duration-500 sm:duration-700"
+									leave="transform transition ease-in-out duration-300 sm:duration-700"
 									leaveFrom="translate-x-0"
 									leaveTo="translate-x-full">
 									<Dialog.Panel className="relative w-screen max-w-md pointer-events-auto">
 										<Transition.Child
 											as={Fragment}
-											enter="ease-in-out duration-500"
+											enter="ease-in-out duration-300"
 											enterFrom="opacity-0"
 											enterTo="opacity-100"
-											leave="ease-in-out duration-500"
+											leave="ease-in-out duration-300"
 											leaveFrom="opacity-100"
 											leaveTo="opacity-0">
 											<div className="flex absolute top-0 left-0 pt-4 pr-2 -ml-8 sm:-ml-10 sm:pr-4"></div>
