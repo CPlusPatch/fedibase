cbb5c42ec073b0c0587e46b117621a8495887a9a
Gaspard Wierzbinski <contact@cpluspatch.com>
2023-02-18 02:11:40 -1000

    fuck this

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,7 +51,7 @@ const Home = () => {
 	}, [client, handlePopState]);
 
 	return (
-		<div className="relative text-base not-italic font-inter bg-dark">
+		<div className="relative bg-dark font-inter">
 			<MetaTags title={`Home · Fedibase`} />
 
 			<Nav />
