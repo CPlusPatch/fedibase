ae7b982161f30897fb37b94de6a21a5942cc9eca
Gaspard Wierzbinski <contact@cpluspatch.com>
2023-02-19 18:25:43 -1000

    Fix incorrect code

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,28 +1,9 @@
-import MetaTags from "components/head/MetaTags";
-import Nav from "components/sidebar/Nav";
-import MainLayout from "components/layout/MainLayout";
-import { Conversation } from "components/feed/Conversation";
+import Home from "pages";
 
 const User = ({ id }) => {
 	return (
-		<div className="relative font-inter bg-dark">
-			<MetaTags title={`Post · Fedibase`} />
-
-			<Nav />
-
-			<MainLayout>
-				<Conversation id={id} />
-			</MainLayout>
-		</div>
+		<Home />
 	);
 };
-
-export function getServerSideProps({ params, req, res }) {
-	return {
-		props: {
-			id: params.id,
-		},
-	};
-}
 
 export default User;
--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,10 +1,3 @@
-import MetaTags from "components/head/MetaTags";
-import Nav from "components/sidebar/Nav";
-import { UserFeed } from "components/feed/UserFeed";
-import { useContext, useEffect, useState } from "react";
-import { AuthContext } from "components/context/AuthContext";
-import { Response, Entity } from "megalodon";
-import MainLayout from "components/layout/MainLayout";
 import Home from "pages";
 
 const User = () => {
