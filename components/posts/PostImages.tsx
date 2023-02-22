7fa30291a27db39205ef81fc6484b6642bcede61
Gaspard Wierzbinski <contact@cpluspatch.com>
2023-02-21 18:29:09 -1000

    Fix PostImages spoiler hide button padding

--- components/posts/PostImages.tsx
+++ components/posts/PostImages.tsx
@@ -66,7 +66,7 @@ function PostImage({ status, image }: { status: Entity.Status; image: Entity.Att
 						onClick={() => {
 							setRevealed(false);
 						}}
-						className="px-1 py-1 mt-4 mr-4">
+						className="!px-1 !py-1 mt-4 mr-4">
 						<IconX aria-hidden={true} className="w-8 h-8" />
 						<span className="sr-only">Hide this image</span>
 					</Button>
