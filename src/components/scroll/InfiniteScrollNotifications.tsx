526d1b91c31f1d33a811887eb6f21931a3c356e6
Gaspard Wierzbinski <contact@cpluspatch.com>
2023-02-22 19:41:32 -1000

    Fix some UI issues

--- src/components/scroll/InfiniteScrollNotifications.tsx
+++ src/components/scroll/InfiniteScrollNotifications.tsx
@@ -34,10 +34,15 @@ export const Notification = ({ entity }: { entity: Entity.Notification }) => {
 					{entity.type == "favourite" && (
 						<a
 							href={`/users/@${entity.account.id}`}
-							className="overflow-hidden gap-x-2 max-w-full text-sm italic text-gray-500 overflow-ellipsis dark:text-gray-400 hover:underline">
+							className="overflow-hidden gap-x-1 flex max-w-full items-center font-semibold text-sm text-gray-500 overflow-ellipsis dark:text-gray-400 hover:underline">
 							<IconStarFilled
 								aria-hidden={true}
 								className="inline mr-1 w-[1em] pb-0.5 text-yellow-500 hover:animate-spin"
+							/>
+							<img
+								src={entity.account.avatar}
+								alt=""
+								className="h-[1em] w-[1em] inline mb-0.5 mr-2 rounded"
 							/>
 							{withEmojis(entity.account.display_name, entity.account.emojis)}{" "}
 							favourited your post
@@ -46,11 +51,11 @@ export const Notification = ({ entity }: { entity: Entity.Notification }) => {
 					{entity.type == "emoji_reaction" && (
 						<a
 							href={`/users/@${entity.account.id}`}
-							className="overflow-hidden gap-x-2 max-w-full text-sm italic text-gray-500 overflow-ellipsis dark:text-gray-400 hover:underline">
+							className="overflow-hidden gap-x-1 items-center flex max-w-full font-semibold text-gray-500 overflow-ellipsis dark:text-gray-400 hover:underline">
 							<img
 								src={entity.account.avatar}
 								alt=""
-								className="h-[1em] w-[1em] inline mb-0.5 mr-1"
+								className="h-[1em] w-[1em] inline rounded mb-0.5 mr-1"
 							/>
 							{withEmojis(entity.account.display_name, entity.account.emojis)} reacted
 							with{" "}
@@ -62,7 +67,7 @@ export const Notification = ({ entity }: { entity: Entity.Notification }) => {
 					{entity.type == "reblog" && (
 						<a
 							href={`/users/@${entity.account.id}`}
-							className="overflow-hidden gap-x-2 max-w-full text-sm italic text-gray-500 overflow-ellipsis dark:text-gray-400 hover:underline">
+							className="overflow-hidden gap-x-1 flex max-w-full items-center font-semibold text-gray-500 overflow-ellipsis dark:text-gray-400 hover:underline">
 							<svg
 								aria-hidden={true}
 								xmlns="http://www.w3.org/2000/svg"
@@ -72,6 +77,11 @@ export const Notification = ({ entity }: { entity: Entity.Notification }) => {
 									fill="currentColor"
 									d="M272 416c17.7 0 32-14.3 32-32s-14.3-32-32-32H160c-17.7 0-32-14.3-32-32V192h32c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-64-64c-12.5-12.5-32.8-12.5-45.3 0l-64 64c-9.2 9.2-11.9 22.9-6.9 34.9S19.1 192 32.1 192h32v128c0 53 43 96 96 96H272zm32-320c-17.7 0-32 14.3-32 32s14.3 32 32 32h112c17.7 0 32 14.3 32 32v128h-32c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l64 64c12.5 12.5 32.8 12.5 45.3 0l64-64c9.2-9.2 11.9-22.9 6.9-34.9S556.9 320 543.9 320h-32V192c0-53-43-96-96-96H304z"></path>
 							</svg>
+							<img
+								src={entity.account.avatar}
+								alt=""
+								className="h-[1em] w-[1em] inline mb-0.5 mr-2 rounded"
+							/>
 							{withEmojis(entity.account.display_name, entity.account.emojis)} boosted
 							your post
 						</a>
