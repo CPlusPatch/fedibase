a3d36e4dd16cacfa511c39999e898f9dc5b1adf5
Gaspard Wierzbinski <contact@cpluspatch.com>
2023-02-25 18:07:46 -1000

    Remove dist/ directory, add PWA support again

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,78 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite';
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact(), tsconfigPaths()],
+	plugins: [
+		preact(),
+		tsconfigPaths(),
+		VitePWA({
+			registerType: "autoUpdate",
+			devOptions: {
+				enabled: true,
+			},
+			manifest: {
+				name: "Fedibase",
+				short_name: "Fedibase",
+				theme_color: "#f38121",
+				background_color: "#181a1b",
+				display: "standalone",
+				orientation: "portrait",
+				scope: "/",
+				start_url: "/",
+				icons: [
+					{
+						src: "images/icons/icon-72x72.png",
+						sizes: "72x72",
+						type: "image/png",
+					},
+					{
+						src: "images/icons/icon-96x96.png",
+						sizes: "96x96",
+						type: "image/png",
+					},
+					{
+						src: "images/icons/icon-128x128.png",
+						sizes: "128x128",
+						type: "image/png",
+					},
+					{
+						src: "images/icons/icon-144x144.png",
+						sizes: "144x144",
+						type: "image/png",
+					},
+					{
+						src: "images/icons/icon-152x152.png",
+						sizes: "152x152",
+						type: "image/png",
+					},
+					{
+						src: "images/icons/icon-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "images/icons/icon-384x384.png",
+						sizes: "384x384",
+						type: "image/png",
+					},
+					{
+						src: "images/icons/icon-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+			},
+		}),
+	],
 	define: {
 		"process.env": {},
 	},
 	resolve: {
 		alias: {
-			querystring: "querystring-es3"
-		}
-	}
+			querystring: "querystring-es3",
+		},
+	},
 });
