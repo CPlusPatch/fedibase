6c2787d3b5229154dca620177c8c0706552b8c8e
Jesse Wierzbinski <contact@cpluspatch.com>
2023-03-05 19:49:15 -1000

    Fix build error

--- src/components/sidebar/LeftSidebar.tsx
+++ src/components/sidebar/LeftSidebar.tsx
@@ -464,7 +464,7 @@ function SendForm() {
 					</div>
 					<div>
 						<Button
-							isLoading={currentState.loading}
+							loading={currentState.loading}
 							style="orangeLight"
 							type="submit"
 							className="!px-4 !py-2 !text-base text-white dark:text-white !border-none !bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] !from-pink-500 !via-red-500 !to-yellow-500">
