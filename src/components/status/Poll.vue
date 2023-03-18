7962bdb222ad543b3460f8e3f7341c837aa8bd43
Jesse Wierzbinski <contact@cpluspatch.com>
2023-03-17 15:44:18 -1000

    Fix and make polls work again

--- src/components/status/Poll.vue
+++ src/components/status/Poll.vue
@@ -2,20 +2,41 @@
 import { Entity } from 'megalodon';
 import Button from '../button/Button.vue';
 import { fromNow } from '../../utils/functions';
+import { store } from '../../utils/store';
+import { ref } from 'vue';
 
 const props = defineProps<{
 	status: Entity.Status,
 }>();
+
+const _poll = ref<Entity.Poll | null>(props.status.poll);
+
+const onSubmit = (e: Event) => {
+	e.preventDefault();
+	const value = [];
+
+	if (!_poll.value) return false;
+
+	for (let i = 0; i < (e.target as any)["poll"].length; i++) {
+		if ((e.target as any)["poll"][i].checked)
+			value.push((e.target as any)["poll"][i].value);
+	}
+	store.client
+		?.votePoll(_poll.value?.id, value, props.status.id)
+		.then(res => {
+			_poll.value = res.data;
+		});
+}
 </script>
 
 <template>
-	<form class="list-inside flex-col gap-y-2 flex" action="#">
-		<li v-for="option, index of status.poll.options" v-if="status.poll"
+	<form class="list-inside flex-col gap-y-2 flex" action="#" @submit="onSubmit">
+		<li v-for="option, index of _poll.options" v-if="_poll"
 			class="flex flex-row gap-x-1 items-center relative dark:text-gray-100" :key="index">
 			<div :style="{
 				width: `${Math.round(
 					((option.votes_count ?? 0) /
-						(status.poll?.votes_count ?? 0)) *
+						(_poll?.votes_count ?? 0)) *
 					100
 				)}%`
 			}" class="absolute max-w-full bg-gradient-to-l from-pink-300 via-purple-300 to-indigo-400 rounded-sm h-full z-0">
@@ -25,30 +46,30 @@ const props = defineProps<{
 				{{ Number.isNaN(
 					Math.round(
 						((option.votes_count ?? 0) /
-							(status.poll?.votes_count ?? 0)) *
+							(_poll?.votes_count ?? 0)) *
 						100
 					)
 				)
 					? 0
 					: Math.round(
 						((option.votes_count ?? 0) /
-							(status.poll?.votes_count ?? 0)) *
+							(_poll?.votes_count ?? 0)) *
 						100
 					) }}%
 			</span>
 
-			<input v-if="!status.poll?.voted" :type="status.poll?.multiple ? 'checkbox' : 'radio'" name="poll"
+			<input v-if="!_poll?.voted" :type="_poll?.multiple ? 'checkbox' : 'radio'" name="poll"
 				class="z-10 focus:outline-none focus:ring-0 rounded outline-none m-0 p-0 mr-2" :value="index" />
 
 			{{ option.title }}
 		</li>
 
 		<div class="text-sm text-gray-500 dark:text-gray-400">
-			<Button theme="gray" type="submit" class="!px-2 !py-1 mr-2" v-if="!status.poll?.voted">
+			<Button theme="gray" type="submit" class="!px-2 !py-1 mr-2" v-if="!_poll?.voted">
 				Vote
 			</Button>
 
-			{{ status.poll?.votes_count }} people voted &middot; {{ status.poll?.expired ? "Poll ended" : "Poll ends" }} {{ fromNow(new Date(status.poll?.expires_at ?? "")) }}
+			{{ _poll?.votes_count }} people voted &middot; {{ _poll?.expired ? "Poll ended" : "Poll ends" }} {{ fromNow(new Date(_poll?.expires_at ?? "")) }}
 		</div>
 	</form>
 </template>
