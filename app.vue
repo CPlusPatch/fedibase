2442747851b1b2d38a58cd5292ef62d23ff0deb8
Jesse Wierzbinski <contact@cpluspatch.com>
2023-04-26 14:21:28 -1000

    Switch to own fedi library to save 300 kb

--- app.vue
+++ app.vue
@@ -1,16 +1,5 @@
 <script setup lang="ts">
 import "./styles/index.css";
-import Client, { ApiType } from "./app/api/Client";
-
-const client = new Client(
-	new URL(store.auth.url),
-	store.auth.token ?? "",
-	ApiType.Pleroma
-);
-
-client.getHomeTimeline().then(res => {
-	console.log(res);
-});
 </script>
 
 <template>
--- app/api/types/entities/status_source.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace Entity {
-    type StatusSource = {
-        id: string;
-        text: string;
-        spoiler_text: string;
-    };
-}
